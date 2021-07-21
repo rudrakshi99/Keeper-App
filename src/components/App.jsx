94cfc82b909ebda2f98077c80e8051ee60cfec2f
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-07-21 11:28:06 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Footer } from './Footer'
+import { Header } from './Header'
+import { Note } from './Note'
+
+export const App = () => {
+    return (
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
+    )
+}
