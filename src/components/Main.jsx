b89d6b420fb0f7b2c6a0ea48582db51d55d51305
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-07-30 21:08:37 +0530

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -20,37 +20,33 @@ export const Main = () => {
         getNotes()
     },[])
 
-    axios.interceptors.response.use(undefined, 
-        
-          function axiosRetryInspector(err)  {
-              const refreshToken = localStorage.getItem('refresh')
-              if (err.response.status === 401 && err.response.data.detail === 'Authentication credentials were not provided.') {
-               console.log("ooo")
-                const response = axios.post(`http://127.0.0.1:8000/api/accounts/token/refresh/`, {
-                  refresh: refreshToken
-                })
-                  .then((res) => res.data)
-                  .then((res) => {
-                    console.log(err.config)
-                    err.config.headers['Authorization'] = 'Bearer ' + res.access;
-                    localStorage.setItem('access', res.access)
-                    // return axios.request(err.config);
+    axios.interceptors.response.use(undefined,         
+      function axiosRetryInspector(err)  {
+          const refreshToken = localStorage.getItem('refresh')
+          if (err.response.status === 401 && err.response.data.detail === 'Authentication credentials were not provided.') {
+            console.log("ooo")
+            axios.post(`http://localhost:8000/api/accounts/token/refresh/`, {
+              refresh: refreshToken
+            })
+              .then((res) => res.data)
+              .then((res) => {
+                console.log(err.config)
+                err.config.headers['Authorization'] = 'Bearer ' + res.access;
+                localStorage.setItem('access', res.access)
+                // return axios.request(err.config);
 
-                    //  return axios(originalRequest)
-                  })
-                // resolve(response)
-              }
-        
-              return Promise.reject(err)
-            }
-          
-        
+                //  return axios(originalRequest)
+              })
+            // resolve(response)
+          }
+    
+          return Promise.reject(err)
+        }            
       );
       
     
       const addNote = (note) => {
-        console.log("fffffff")
-        
+        console.log("fffffff")        
         const headers = { 
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + localStorage.getItem('access')
