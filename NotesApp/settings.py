beffd06659885e4e16f26b164bba5d12c26dbe8e
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-07-31 12:19:58 +0530

    changed permission class

--- NotesApp/settings.py
+++ NotesApp/settings.py
@@ -84,9 +84,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'NotesApp.wsgi.application'
 
 REST_FRAMEWORK = {
-        'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+
     'DEFAULT_FILTER_BACKENDS': 
         ['django_filters.rest_framework.DjangoFilterBackend']
 
