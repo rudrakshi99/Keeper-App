00914cab906a6db6d2ef103d4eec97fb17bf7f8b
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-08-03 17:41:58 +0530

    update the response time

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,161 +1,156 @@
-import React , {useState, useEffect} from 'react'
-import { Footer } from './pages/Footer'
-import { Header } from './pages/Header'
-import { Notes } from './pages/Notes'
-import axios from 'axios'
-import { CreateArea } from './pages/CreateArea'
-import { useHistory } from 'react-router-dom'
-import  FormDialog  from './pages/Popup'
-import { Loading } from './pages/Loading'
+import React, { useState, useEffect } from "react";
+import { Footer } from "./pages/Footer";
+import { Header } from "./pages/Header";
+import { Notes } from "./pages/Notes";
+import axios from "axios";
+import { CreateArea } from "./pages/CreateArea";
+import { useHistory } from "react-router-dom";
+import FormDialog from "./pages/Popup";
+import { Loading } from "./pages/Loading";
 
 export const Main = () => {
-    const history = useHistory()
-    const [notes, setNotes] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
+  const history = useHistory();
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [response, setResponse ] = useState(null);
 
-    useEffect(()=>{
-        const getNotes = () => {
-          setIsLoading(false)
-          const headers = { 
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + localStorage.getItem('access')
-          }
-
-            axios.get(`http://127.0.0.1:8000/api/notes/` , {headers} )
-            .then((res)=>{ 
-              setNotes(res.data)
-              setIsLoading(true)
-            })
-            .catch(err=>console.log(err))
-          }
-          getNotes()
-        
-    },[])
-
-    axios.interceptors.response.use(undefined,         
-      function axiosRetryInspector(err)  {
-          const refreshToken = localStorage.getItem('refresh')
-          console.log(refreshToken)
-          if (err.response.status === 401 && refreshToken) {
-            console.log("ooo")
-            axios.post(`http://localhost:8000/api/accounts/token/refresh/`, {
-              refresh: refreshToken
-            })
-              .then((res) => res.data)
-              .then((res) => {
-                
-                // console.log(err.config)
-                err.config.headers['Authorization'] = 'Bearer ' + res.access;
-                localStorage.setItem('access', res.access)
-                return axios.request(err.config);
-
-              })
-              
-          } else if(err.response.status === 400) {    
-              history.push("/")
-          }
-    
-          return Promise.reject(err)
-        }            
-      );
-      
-    
-      const addNote = (note) => {
-        // console.log("fffffff")    
-        setIsLoading(false)    
-        const headers = { 
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + localStorage.getItem('access')
-        }
-        console.log(headers)
-        axios.post(`http://127.0.0.1:8000/api/notes/create/`, note, { headers } )
-        .then(()=> {
-        window.location.reload()})
-        .catch(err=>console.log(err))
-        
-        // setNotes([...notes, note])
-        
-
-    }
-    
-    const deleteNote = (id) => {
-      const headers = { 
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + localStorage.getItem('access')
-        }
-        axios.delete(`http://127.0.0.1:8000/api/notes/${id}/`, { headers })
-        .then(res=> res.data)
-        .catch(err=>console.log(err))
-
-        setNotes(notes.filter(note=>note.id !== id))
-
-    }
-    
-    const updateNote = (note) => {
-      console.log(updateId, 'updateId')
-      setOpen(false);
-      setIsLoading(false) 
-
-       const headers = { 
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + localStorage.getItem('access')
-        }
-        axios.put(`http://127.0.0.1:8000/api/notes/${updateId}/`, note , { headers })
+  useEffect(() => {
+    setIsLoading(true);
+    const getNotes = () => {      
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("access"),
+      };
+      axios
+        .get(`http://127.0.0.1:8000/api/notes/`, { headers })
         .then((res) => {
-          // setIsLoading(true) 
-          console.log(note, 'note') 
-          window.location.reload();
-          
+            setIsLoading(false);
+          setNotes(res.data);          
         })
-        .catch(err=>console.log(err))
-      
-        // setNotes(notes.map(note=>note.id === updateId ? note = note : note))
-        // setNotes(notes.filter(note=>note.id !== updateId))
-        console.log("kk")
-        
-
-      
-    }
-
-    const logout = () => {
-
-      const token = {
-        'refresh' : localStorage.getItem('refresh')
-      }
-      const headers = { 
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + localStorage.getItem('access')
-        }
-      axios.post('http://localhost:8000/api/accounts/logout/', token , { headers })
-      .then(()=>{
-
-        localStorage.removeItem('access')
-        localStorage.removeItem('refresh')
-        localStorage.removeItem('username')
-        history.push('/')
-      })
-      .catch(err=>console.log(err))
-    }
-
-
-    const [open, setOpen] = useState(false);
-    const [updateId, setUpdateId] = useState('');
-
-    const handleClickOpen = () => {
-      console.log(open, 'open')
-      
-      setOpen(true);
+        .catch((err) => { err.response.status === 401 && window.location.reload() });
     };
+    getNotes();
+  }, [response]);
 
-    return (
-        <div>
-            <Header logout={logout}/>
-            <CreateArea onAdd={addNote} />
-            { !isLoading ? <Loading /> : <Notes notes={notes} onDelete={deleteNote} OpenPopup={handleClickOpen} 
-                setUpdateId={setUpdateId}
-            />}
-            <FormDialog open={open} setOpen={setOpen} updateNote={updateNote} />
-            <Footer />
-        </div>
-    )
-}
+  axios.interceptors.response.use(undefined, function axiosRetryInspector(err) {
+    const refreshToken = localStorage.getItem("refresh");
+    console.log(refreshToken);
+    if (err.response.status === 401 && refreshToken) {      
+      axios
+        .post(`http://localhost:8000/api/accounts/token/refresh/`, {
+          refresh: refreshToken,
+        })
+        .then((res) => res.data)
+        .then((res) => {
+          err.config.headers["Authorization"] = "Bearer " + res.access;
+          localStorage.setItem("access", res.access);
+          return axios.request(err.config);
+        });
+    } else if (err.response.status === 400) {
+      history.push("/");
+    }
+    return Promise.reject(err);
+  });
+
+  const addNote = (note) => { 
+    setIsLoading(true);
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("access"),
+    };
+    axios
+      .post(`http://127.0.0.1:8000/api/notes/create/`, note, { headers })
+      .then((res) => {
+        // setNotes([...notes, note])
+        setResponse(res.data);
+        // setNotes((prevState) => prevState.push(res.data));
+      })
+      .catch((err) => console.log(err));
+
+    
+  };
+
+  const deleteNote = (id) => {
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("access"),
+    };
+    axios
+      .delete(`http://127.0.0.1:8000/api/notes/${id}/`, { headers })
+      .then((res) => res.data)
+      .catch((err) => console.log(err));
+    setNotes(notes.filter((note) => note.id !== id));
+  };
+
+  const updateNote = (note) => {
+    console.log(updateId, "updateId");
+    setOpen(false);
+    setIsLoading(true);
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("access"),
+    };
+    axios
+      .put(`http://127.0.0.1:8000/api/notes/${updateId}/`, note, { headers })
+      .then((res) => {    
+        
+        console.log(note, "note");
+        
+        setNotes(notes.filter(note=>note.id !== updateId))  
+        setResponse(res.data);
+        
+      })
+      .catch((err) => console.log(err));
+   
+    console.log("kk");
+  };
+
+  const logout = () => {
+    const token = {
+      refresh: localStorage.getItem("refresh"),
+    };
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("access"),
+    };
+    axios
+      .post("http://localhost:8000/api/accounts/logout/", token, { headers })
+      .then(() => {
+        localStorage.removeItem("access");
+        localStorage.removeItem("refresh");
+        localStorage.removeItem("username");
+        history.push("/");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const [open, setOpen] = useState(false);
+  const [updateId, setUpdateId] = useState("");
+
+  const handleClickOpen = () => {
+    console.log(open, "open");
+
+    setOpen(true);
+  };
+
+  return (
+    <div>
+      <Header logout={logout} />
+      <CreateArea onAdd={addNote} />
+      <div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Notes
+          notes={notes}
+          onDelete={deleteNote}
+          OpenPopup={handleClickOpen}
+          setUpdateId={setUpdateId}
+        />
+      )}
+      </div>
+      <FormDialog open={open} setOpen={setOpen} updateNote={updateNote} />
+      <Footer />
+    </div>
+  );
+};
